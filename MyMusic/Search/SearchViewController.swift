7ec0971e4cf5a01bb763b25067107f0cb22dddbe
NosovaDaria <kuleshovadasha@gmail.com>
2022-12-30 11:53:01 +0300

    Search Delay fixing

--- MyMusic/Search/SearchViewController.swift
+++ MyMusic/Search/SearchViewController.swift
@@ -24,6 +24,8 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     let searchController = UISearchController(searchResultsController: nil)
     private var searchViewModel = SearchViewModel.init(cells: [])
     private var timer: Timer?
+    
+    private lazy var footerView = FooterView()
 
   // MARK: Object lifecycle
   
@@ -77,16 +79,19 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
         
         let nib = UINib(nibName: "TrackCell", bundle: nil)
         table.register(nib, forCellReuseIdentifier: TrackCell.reuseId)
+        table.tableFooterView = footerView
     }
   
   func displayData(viewModel: Search.Model.ViewModel.ViewModelData) {
       switch viewModel {
-          case .some:
-              print("viewController .some")
           case .displayTracks(let searchViewModel):
               print("viewController .displayTracks")
               self.searchViewModel = searchViewModel
               table.reloadData()
+              footerView.hideLoader()
+              print("footerView hided")
+          case .displayFooterView:
+              footerView.showLoader()
       }
 
   }
@@ -103,7 +108,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = table.dequeueReusableCell(withIdentifier: TrackCell.reuseId, for: indexPath) as! TrackCell
         let cellViewModel = searchViewModel.cells[indexPath.row]
-        print("cellViewModel.previewUrl:", cellViewModel.previewUrl)
+        print("cellViewModel.previewUrl:", cellViewModel.previewUrl ?? "")
         cell.trackImageView.backgroundColor = .red
         cell.set(viewModel: cellViewModel)
 
@@ -113,8 +118,21 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 84
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Please enter search term above..."
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        return label
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        searchViewModel.cells.count > 0 ? 0 : 250
+    }
 }
 
+// MARK: - UISearchBarDelegate
 extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
